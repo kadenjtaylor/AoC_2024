c7edf193bdd0f7977d8373a58526c89595095da5
Kaden Taylor <kadenjtaylor@gmail.com>
2025-09-02 11:20:14 -0700

    cleanup

--- problems/src/days/Day_8.scala
+++ problems/src/days/Day_8.scala
@@ -30,48 +30,54 @@ object Day_8 extends Day {
 
   case class Location(r: Int, c: Int) {
     override def toString(): String = s"($r, $c)"
-    def minus(rDiff: Int, cDiff: Int) = {
-      Location(r - rDiff, c - cDiff)
+    def minus(loc: Location) = {
+      Location(r - loc.r, c - loc.c)
     }
 
-    def plus(rDiff: Int, cDiff: Int) = {
-      Location(r + rDiff, c + cDiff)
+    def plus(loc: Location) = {
+      Location(r + loc.r, c + loc.c)
     }
 
     def scale(loc: Location): Option[Int] = {
       val rScale = loc.r / r
       val cScale = loc.c / c
-      if rScale == cScale then
-        Some(rScale)
-      else
-        None
+      if rScale == cScale then Some(rScale)
+      else None
     }
   }
 
-  case class AntennaMap(nodes: Map[Char, Set[Location]], farCorner: Location)
-
-  /*  */
-  def antinodes(m: AntennaMap, char: Char, resonant: Boolean = false): Set[Location] = {
-    m.nodes
-      .getOrElse(char, Set())
-      .toSeq
-      .combinations(2)
-      .flatMap(locs => antinodesFor(locs(0), locs(1), m.farCorner, resonant))
-      .toSet
+  case class AntennaMap(nodes: Map[Char, Set[Location]], farCorner: Location) {
+    
+    def allAntinodes(resonant: Boolean = false): Set[Location] =
+      nodes.keySet.flatMap(c => antinodesForChar(c, resonant)).toSet
+  
+    private def antinodesForChar(char: Char, resonant: Boolean): Set[Location] = {
+      nodes
+        .getOrElse(char, Set())
+        .toSeq
+        .combinations(2)
+        .flatMap(locs => antinodesFor(locs(0), locs(1), farCorner, resonant))
+        .toSet
+    }
   }
 
-  def antinodesFor(l1: Location, l2: Location, farCorner: Location, resonant: Boolean): List[Location] = {
+  private def antinodesFor(
+      l1: Location,
+      l2: Location,
+      farCorner: Location,
+      resonant: Boolean
+  ): List[Location] = {
     val rDiff = l2.r - l1.r
     val cDiff = l2.c - l1.c
     val back = List.unfold(l1)(loc => {
       if inBounds(loc, farCorner.r, farCorner.c) then
-        val nextLocation: Location = loc.minus(rDiff, cDiff)
+        val nextLocation: Location = loc.minus(Location(rDiff, cDiff))
         Some(loc, nextLocation)
       else None
     })
     val forth = List.unfold(l2)(loc => {
       if inBounds(loc, farCorner.r, farCorner.c) then
-        val nextLocation: Location = loc.plus(rDiff, cDiff)
+        val nextLocation: Location = loc.plus(Location(rDiff, cDiff))
         Some(loc, nextLocation)
       else None
     })
@@ -79,14 +85,12 @@ object Day_8 extends Day {
       (back ++ forth)
         .filter(loc => loc != l1 && loc != l2)
         .filter(loc => {
-          val l1Dist = l1.minus(loc.r, loc.c)
-          val l2Dist = l2.minus(loc.r, loc.c)
+          val l1Dist = l1.minus(loc)
+          val l2Dist = l2.minus(loc)
           l1Dist.scale(l2Dist) == Some(2) || l2Dist.scale(l1Dist) == Some(2)
         })
-    else
-      (back ++ forth)
+    else (back ++ forth)
   }
-
 
   def inBounds(loc: Location, rMax: Int, cMax: Int): Boolean =
     0 <= loc.r && loc.r <= rMax && 0 <= loc.c && loc.c <= cMax
@@ -116,20 +120,20 @@ object Day_8 extends Day {
   }
 
   override def example: Unit = {
-    val m = Parsing.parse(exampleData)
-    val ans = m.nodes.keySet.flatMap(c => antinodes(m, c)).toSet
+    val m   = Parsing.parse(exampleData)
+    val ans = m.allAntinodes()
     println(s"Found ${ans.size} unique antinodes")
   }
 
   override def part1: Unit = {
-    val m = Parsing.parse(Utils.readDailyResourceIntoString(8))
-    val ans = m.nodes.keySet.flatMap(c => antinodes(m, c)).toSet
+    val m   = Parsing.parse(Utils.readDailyResourceIntoString(8))
+    val ans = m.allAntinodes()
     println(s"Found ${ans.size} unique antinodes")
   }
 
   override def part2: Unit = {
-    val m = Parsing.parse(Utils.readDailyResourceIntoString(8))
-    val ans = m.nodes.keySet.flatMap(c => antinodes(m, c, true)).toSet
+    val m   = Parsing.parse(Utils.readDailyResourceIntoString(8))
+    val ans = m.allAntinodes(true)
     println(s"Found ${ans.size} unique antinodes")
   }
 }
