bee2a14bfa0a9dc334184c602148a44797b5050b
Kaden Taylor <kadenjtaylor@gmail.com>
2025-09-05 15:30:51 -0700

    Day 9 - Part 2

--- problems/src/days/Day_9.scala
+++ problems/src/days/Day_9.scala
@@ -4,6 +4,7 @@ import model.Day
 import model.Utils
 import scala.collection.mutable.ArrayBuffer
 import fastparse.internal.Util
+import days.Day_9.Block.Empty
 
 case object Day_9 extends Day {
 
@@ -46,8 +47,75 @@ case object Day_9 extends Day {
     }
   }
 
+  trait Sized {
+    def size: Int
+  }
+
+  enum Block extends Sized:
+    case File(id: Int, size: Int)
+    case Empty(size: Int)
+
+    def isEmpty: Boolean = this match
+      case File(_, _) => false
+      case Empty(_)   => true
+
+  case class FileDrive(ab: ArrayBuffer[Block]) {
+
+    override def toString(): String =
+      "|" + ab.map(b => if b.isEmpty then "." * b.size else "*" * b.size).mkString("|") + "|"
+
+    def defrag() = {
+      //   println(this.toDrive().toString())
+      var rPtr = ab.length - 1
+      while (rPtr > 0) {
+        // Move rPtr onto a non-empty
+        while (ab(rPtr).isEmpty) {
+          rPtr -= 1
+        }
+        val n = ab(rPtr)
+        // println(s"Need to find a place to put: $n")
+
+        val firstEmptyOfCorrectSize =
+          ab.zipWithIndex
+            .collectFirst({
+              case (Block.Empty(size), index) if index < rPtr && size >= n.size => (index, size)
+            })
+
+        // println(s"Found: $firstEmptyOfCorrectSize")
+
+        firstEmptyOfCorrectSize match
+          case Some((emptyIndex, emptySize)) => {
+            // println(s"Setting index $emptyIndex to $n")
+            ab.update(emptyIndex, n)
+            // println(s"Setting index $rPtr to Empty(${n.size})")
+            ab.update(rPtr, Block.Empty(n.size))
+            // println(s"Inserting remaining extra space after file")
+            ab.insert(emptyIndex + 1, Empty(emptySize - n.size))
+            // println(s"Adding +1 to rPtr to counter longer list")
+            rPtr += 1
+          }
+          case None => {
+            // println(s"Skipping $n")
+          }
+
+        rPtr -= 1
+        // println(this.toDrive().toString())
+
+      }
+    }
+    def toDrive() = {
+      Drive(
+        ab.flatMap(block =>
+          block match
+            case Block.File(id, size) => List.fill(size)(id)
+            case Block.Empty(size)    => List.fill(size)(-1)
+        )
+      )
+    }
+  }
+
   object Parsing {
-    def parse(s: String) =
+    def parse(s: String): Drive =
       val a = ArrayBuffer[Int]()
       //   println(s"Parsing string of length: ${s.length()}")
       s.toCharArray
@@ -69,6 +137,21 @@ case object Day_9 extends Day {
           }
         })
       Drive(a)
+
+    def parse2(s: String): FileDrive =
+      val a = ArrayBuffer[Block]()
+      s.toCharArray
+        .grouped(2)
+        .zipWithIndex
+        .foreach((arr, id) => {
+          val fileSize = arr(0).toString().toInt
+          a.addOne(Block.File(id, fileSize))
+          if (arr.length == 2) {
+            var emptySize = arr(1).toString().toInt
+            a.addOne(Block.Empty(emptySize))
+          }
+        })
+      FileDrive(a)
   }
 
   override def example: Unit = {
@@ -81,6 +164,12 @@ case object Day_9 extends Day {
     val drive = Parsing.parse(Utils.readDailyResourceIntoString(9))
     drive.defrag()
     println(drive.checksum())
+  }
+
+  override def part2: Unit = {
+    val fileDrive = Parsing.parse2(Utils.readDailyResourceIntoString(9))
+    fileDrive.defrag()
+    println(fileDrive.toDrive().checksum())
   }
 
 }
