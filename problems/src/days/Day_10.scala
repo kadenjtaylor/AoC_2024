4d4fa952b05ce12215e5b5757a0c0ae7b2c09742
Kaden Taylor <kadenjtaylor@gmail.com>
2025-09-08 17:39:11 -0700

    WIP

--- problems/src/days/Day_10.scala
+++ problems/src/days/Day_10.scala
@@ -9,7 +9,9 @@ case object Day_10 extends Day {
                               |8765
                               |9876""".stripMargin
 
-  case class Location(r: Int, c: Int)
+  case class Location(r: Int, c: Int) {
+    override def toString(): String = s"($r, $c)"
+  }
 
   type Height = Int
 
@@ -17,6 +19,29 @@ case object Day_10 extends Day {
     def trailheads() = tiles
       .filter((loc, height) => height == 0)
       .map((loc, _) => loc)
+
+    def stepsFrom(loc: Location) =
+      tiles.get(loc) match
+        case None => List()
+        case Some(height) =>
+          List(
+            Location(loc.r + 1, loc.c),
+            Location(loc.r - 1, loc.c),
+            Location(loc.r, loc.c + 1),
+            Location(loc.r, loc.c - 1)
+          ).flatMap(loc =>
+            tiles.get(loc) match
+              case Some(nextheight) if nextheight - height == 1 => Some(loc)
+              case _                                            => None
+          )
+
+    def allTrails() =
+      var trails = trailheads().map(List(_)).toList
+      var done     = false
+      while (!done) {
+        // TODO: Grow each list where possible (branching)
+        // Stop condition is that each trail is terminated at a 9
+      }
   }
 
   object TopographicalMap {
@@ -39,8 +64,11 @@ case object Day_10 extends Day {
   override def example: Unit = {
     val terrain    = TopographicalMap.parse(exampleData)
     val trailheads = terrain.trailheads()
+    println(exampleData)
     println(s"trailheads at:")
-    trailheads.foreach(println(_))
+    trailheads.foreach(loc =>
+      println(s"Trailhead: $loc => [${terrain.stepsFrom(loc).mkString(", ")}]")
+    )
   }
 
 }
