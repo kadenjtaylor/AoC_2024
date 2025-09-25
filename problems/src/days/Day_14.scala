39e2b2bf980b6ae176abc9d188f8b11d51673246
Kaden Taylor <kadenjtaylor@gmail.com>
2025-09-25 16:30:37 -0700

    Day 14 example

--- problems/src/days/Day_14.scala
+++ problems/src/days/Day_14.scala
@@ -31,11 +31,22 @@ object Day_14 extends Day {
                               |p=2,4 v=2,-3
                               |p=9,5 v=-3,-3""".stripMargin
 
+  private val singleRobot = "p=2,4 v=2,-3"
+
   case class Pair(r: Int, c: Int)
+
+  private def wrap(n: Int, multiple: Int): Int =
+    if n >= 0 && n < multiple then n
+    else if n < 0 then
+      val magicNumber: Int = ((0 - n) / multiple) + 1
+      (n + (magicNumber * multiple)) % multiple
+    else n % multiple
 
   case class Robot(position: Pair, velocity: Pair) {
     def positionAfter(seconds: Int, maxRows: Int, maxCols: Int): Pair = {
-      ??? // TODO: Theoretically we have all the info right here
+      val r = wrap(position.r + (velocity.r * seconds), maxRows)
+      val c = wrap(position.c + (velocity.c * seconds), maxCols)
+      Pair(r, c)
     }
   }
 
@@ -55,12 +66,16 @@ object Day_14 extends Day {
 
   case class Environment(robots: Seq[Robot], rows: Int = 103, cols: Int = 101) {
     override def toString(): String =
-      (0 to rows)
+      render()
+
+    def render(quandrants: Boolean = false) =
+      Range(0, rows)
         .map(row =>
-          (0 to cols)
+          Range(0, cols)
             .map(col => {
               val botCount = robots.count(robot => robot.position == Pair(row, col))
-              if botCount == 0 then "."
+              if quandrants && (row == (rows - 1) / 2 || col == (cols - 1) / 2) then " "
+              else if botCount == 0 then "."
               else botCount.toString()
             })
             .mkString
@@ -73,10 +88,36 @@ object Day_14 extends Day {
         rows,
         cols
       )
+
+    private def robotsIn(topLeft: Pair, bottomRight: Pair): Int =
+      Range(topLeft.r, bottomRight.r)
+        .flatMap(row =>
+          Range(topLeft.c, bottomRight.c)
+            .map(col => {
+              robots.count(robot => robot.position == Pair(row, col))
+            })
+        )
+        .sum
+
+    def safetyFactor: Int =
+      val halfRow = (rows - 1) / 2
+      val halfCol = (cols - 1) / 2
+      val nw      = robotsIn(Pair(0, 0), Pair(halfRow, halfCol))
+      val ne      = robotsIn(Pair(0, halfCol + 1), Pair(halfRow, cols))
+      val sw      = robotsIn(Pair(halfRow + 1, 0), Pair(rows, halfCol))
+      val se      = robotsIn(Pair(halfRow + 1, halfCol + 1), Pair(rows, cols))
+      println(s"NW: $nw / NE: $ne / SW: $sw / SE: $se")
+      nw * ne * sw * se
   }
 
   override def example: Unit =
     val robots = Robots.parse(exampleData)
-    val env    = Environment(robots, rows = 7, cols = 11)
-    println(env.toString())
+    // val fakeRobots = Range(0, 11).flatMap(c => Range(0, 7).map(r => Robot(Pair(r, c), Pair(0, 0))))
+    val env     = Environment(robots, rows = 7, cols = 11)
+    val updated = env.stateAfter(100)
+    println(updated)
+    println()
+    println(updated.render(true))
+    println(s"Safety Factor after 100 seconds: ${updated.safetyFactor}")
+
 }
